970f115cc2e733624a654d23736c0d3097f822f3
Ivan Ermolaev <ermolaevym@gmail.com>
2017-06-14 20:34:21 +0300

    new things

--- main.go
+++ main.go
@@ -10,25 +10,67 @@ import (
 	"github.com/disintegration/imaging"
 )
 
+type ResizePoint struct {
+	width int
+	heigh int
+}
+
 func getMillis(endTime int64) float64 {
 	return float64(endTime) * 0.001 * 0.001
 }
 
-func convertImage(input, output string, quality int) {
-	fImg1, _ := imaging.Open(input)
+const MAX_WIDTH int = 2000
+const MAX_HEIGHT int = 2000
 
-	m := imaging.Resize(fImg1, 300, 0, imaging.Lanczos)
+// func getAvgColor(img Image) {
+//
+// }
+
+func createPointer(width, height int) ResizePoint {
+	return ResizePoint{width, height}
+}
+
+func convertImage(input, output string, quality int) {
+	inputImage, _ := imaging.Open(input)
+
+	var resizeValue ResizePoint = createPointer(23, 23)
+	if inputImage.Bounds().Dx() > inputImage.Bounds().Dy() {
+		if inputImage.Bounds().Dx() > MAX_WIDTH {
+			resizeValue.width = MAX_WIDTH
+		}
+	} else {
+		if inputImage.Bounds().Dy() > MAX_HEIGHT {
+			resizeValue.heigh = MAX_HEIGHT
+		}
+	}
+
+	m := imaging.Resize(inputImage, resizeValue.width, resizeValue.heigh, imaging.Lanczos)
+	m = imaging.Sharpen(m, 0.2)
+	m = imaging.AdjustBrightness(m, 1)
+	m = imaging.AdjustContrast(m, 1)
+
+	outputImage, _ := os.Create(output)
+	defer outputImage.Close()
+
+	//imaging.Save(m, output)
+	jpeg.Encode(outputImage, m, &jpeg.Options{Quality: quality})
+}
+
+func convertSmallImage(input, output string, quality int) {
+	inputImage, _ := imaging.Open(input)
+
+	m := imaging.Resize(inputImage, 300, 0, imaging.Lanczos)
 	m = imaging.Fill(m, 200, 200, imaging.Center, imaging.Lanczos)
 	m = imaging.Sharpen(m, 1.2)
 	m = imaging.AdjustBrightness(m, 3)
 	m = imaging.AdjustContrast(m, 1)
 	m = imaging.AdjustGamma(m, 1.0)
 
-	toimg, _ := os.Create(output)
-	defer toimg.Close()
+	outputImage, _ := os.Create(output)
+	defer outputImage.Close()
 
 	//imaging.Save(m, output)
-	jpeg.Encode(toimg, m, &jpeg.Options{Quality: quality})
+	jpeg.Encode(outputImage, m, &jpeg.Options{Quality: quality})
 }
 
 func printHelp() {
